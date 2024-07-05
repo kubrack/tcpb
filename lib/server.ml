04066ec437ae5431db26db3fabda37a6162bfd33
Oleksandr Kubrak <kubrack@gmail.com>
2024-07-05 17:14:41 +0200

    log implemented

--- lib/server.ml
+++ lib/server.ml
@@ -4,7 +4,7 @@ open Printf
 let backlog = 10
 
 let log_ch = open_out "log"
-let _log line = fprintf log_ch line
+let log line = fprintf log_ch "%s\n%!" line
 
 let listen_sock host port =
   let ip_addr = (gethostbyname host).h_addr_list.(0) in
@@ -12,18 +12,18 @@ let listen_sock host port =
   let listen_sock = socket PF_INET SOCK_STREAM 0 in
   let () = bind listen_sock sock_addr in
   let () = listen listen_sock backlog in
-  let () = eprintf "srv: listening at %s:%d\n%!" (string_of_inet_addr ip_addr) port in
+  let () = eprintf "srv : listening at %s:%d\n%!" (string_of_inet_addr ip_addr) port in
   listen_sock
 
 let int_of_file_descr : Unix.file_descr -> int = Obj.magic (* FIXME *)
 
 let add_client fd _out_chan = 
-  eprintf "srv: adding fd %d\n%!" (int_of_file_descr fd)
+  eprintf "srv : adding fd %d\n%!" (int_of_file_descr fd)
 
 let rm_client ex fd = 
   match ex with
   | Unix_error (unix_err, _syscall, _where) -> begin
-      let () = eprintf "srv: closing %d: %s\n%!" (int_of_file_descr fd) 
+      let () = eprintf "srv : closing %d: %s\n%!" (int_of_file_descr fd) 
         (error_message unix_err) in
       let () = close fd in
       Lwt.return_none
@@ -41,13 +41,14 @@ let start host port =
       | ADDR_INET (ip, p) -> string_of_inet_addr ip ^ ":" ^ string_of_int p
       | ADDR_UNIX s -> s
     in
-    let () = eprintf "srv: accepted: %s as %d\n%!" msg (int_of_file_descr unix_fd) in
+    let _ = log
+      (sprintf "client restarted, accepted from %s as fd %d" msg (int_of_file_descr unix_fd)) in
     let in_chan = Lwt_io.of_unix_fd ~mode:Lwt_io.Input unix_fd in
     let out_chan = Lwt_io.of_unix_fd ~mode:Lwt_io.Output unix_fd in
     let () = add_client unix_fd out_chan in
     let p_read = (fun () -> Lwt_io.read_line in_chan) in
     let rec cb_read line = begin
-      let _ = Lwt_io.write_line (Lwt_io.of_fd ~mode:Lwt_io.Output Lwt_unix.stdout) line  (* TODO to log *) in
+      let _ = log line in
       Lwt.try_bind p_read cb_read (fun ex -> rm_client ex unix_fd)
     end in
     let _ = Lwt.try_bind p_read cb_read (fun ex -> rm_client ex unix_fd) in
